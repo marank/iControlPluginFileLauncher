deca3a9a8aafa2b7a2e6a53f95a27a54ec8488a9
Matthias Rank <mail@m-rank.de>
2015-05-23 20:28:17 +0200

    Initial commit

---
+++ iControlPluginFileLauncher/iControlPluginFileLauncher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using iControlPluginInterface;
+
+namespace iControlPluginFileLauncher {
+    class iControlPlugin : IiControlPlugin {
+        public string Name {
+            get {
+                return "FileLauncher";
+            }
+        }
+
+        public string Author {
+            get {
+                return "Matthias Rank";
+            }
+        }
+
+        private iControlPluginInterface.IiControlPluginHost pluginHost;
+        public iControlPluginInterface.IiControlPluginHost Host {
+            set {
+                pluginHost = value;
+            }
+            get {
+                return pluginHost;
+            }
+        }
+
+
+        public void Handle(string[] commands, string ip) {
+            foreach (string path in System.IO.Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\exec")) {
+                System.IO.FileInfo file = new System.IO.FileInfo(path);
+
+                if (System.IO.Path.GetFileNameWithoutExtension(path).Equals(commands[0])) {
+                    System.Diagnostics.Process.Start(path, String.Join(" ", commands, 1, commands.Length - 1));
+                    pluginHost.Log("Launched " + path, this);
+                }
+            }
+        }
+    }
+}
