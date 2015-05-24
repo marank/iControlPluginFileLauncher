6de5142195c81b5b0cb7aa086309dbc097bebdc4
Matthias Rank <mail@m-rank.de>
2015-05-25 01:41:06 +0200

    Updated iControlInterfaces, added response to client

--- iControlPluginFileLauncher/iControlPluginFileLauncher.cs
+++ iControlPluginFileLauncher/iControlPluginFileLauncher.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using iControlPluginInterface;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using iControlInterfaces;
 
 namespace iControlPluginFileLauncher {
-    class iControlPlugin : IiControlPlugin {
+    public class iControlPlugin : IiControlPlugin {
         public string Name {
             get {
                 return "FileLauncher";
@@ -16,8 +19,8 @@ namespace iControlPluginFileLauncher {
             }
         }
 
-        private iControlPluginInterface.IiControlPluginHost pluginHost;
-        public iControlPluginInterface.IiControlPluginHost Host {
+        private IiControlPluginHost pluginHost;
+        public IiControlPluginHost Host {
             set {
                 pluginHost = value;
             }
@@ -46,17 +49,22 @@ namespace iControlPluginFileLauncher {
             return true;
         }
 
-        public void Handle(string[] commands, string ip) {
+        public void Handle(string[] commands,  IiControlClient client) {
             if (commands[0] == "launch") {
+                NetworkStream clientStream = client.TCP.GetStream();
+                ASCIIEncoding encoder = new ASCIIEncoding();
                 foreach (string file in System.IO.Directory.GetFiles(Path)) {
                     System.IO.FileInfo fi = new System.IO.FileInfo(file);
 
                     if (System.IO.Path.GetFileNameWithoutExtension(file).Equals(commands[1]) || System.IO.Path.GetFileName(file).Equals(commands[1])) {
                         System.Diagnostics.Process.Start(file, String.Join(" ", commands, 2, commands.Length - 2));
                         pluginHost.Log("Launched " + file, this);
+                        byte[] buffer = encoder.GetBytes("Launched " + file);
+                        clientStream.Write(buffer, 0, buffer.Length);
+                        clientStream.Flush();
                     }
                 }
+            }
         }
-          }
     }
 }
